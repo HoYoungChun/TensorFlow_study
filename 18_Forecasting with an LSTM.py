1c31215bc18a7777a25167d051a8fbe44a69882a
HoYoungChun <ho723@naver.com>
2020-10-08 02:50:19 +0900

    Create 18_Forecasting with an LSTM.py

---
+++ 18_Forecasting with an LSTM.py
@@ -0,0 +1,134 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import tensorflow as tf
+
+keras = tf.keras
+
+def plot_series(time, series, format="-", start=0, end=None, label=None):
+    plt.plot(time[start:end], series[start:end], format, label=label)
+    plt.xlabel("Time")
+    plt.ylabel("Value")
+    if label:
+        plt.legend(fontsize=14)
+    plt.grid(True)
+    
+def trend(time, slope=0):
+    return slope * time
+  
+  
+def seasonal_pattern(season_time):
+    """Just an arbitrary pattern, you can change it if you wish"""
+    return np.where(season_time < 0.4,
+                    np.cos(season_time * 2 * np.pi),
+                    1 / np.exp(3 * season_time))
+
+  
+def seasonality(time, period, amplitude=1, phase=0):
+    """Repeats the same pattern at each period"""
+    season_time = ((time + phase) % period) / period
+    return amplitude * seasonal_pattern(season_time)
+  
+  
+def white_noise(time, noise_level=1, seed=None):
+    rnd = np.random.RandomState(seed)
+    return rnd.randn(len(time)) * noise_level
+  
+
+def sequential_window_dataset(series, window_size):
+    series = tf.expand_dims(series, axis=-1)
+    ds = tf.data.Dataset.from_tensor_slices(series)
+    ds = ds.window(window_size + 1, shift=window_size, drop_remainder=True)
+    ds = ds.flat_map(lambda window: window.batch(window_size + 1))
+    ds = ds.map(lambda window: (window[:-1], window[1:]))
+    return ds.batch(1).prefetch(1)
+    
+time = np.arange(4 * 365 + 1)
+
+slope = 0.05
+baseline = 10
+amplitude = 40
+series = baseline + trend(time, slope) + seasonality(time, period=365, amplitude=amplitude)
+
+noise_level = 5
+noise = white_noise(time, noise_level, seed=42)
+
+series += noise
+
+plt.figure(figsize=(10, 6))
+plot_series(time, series)
+plt.show()
+
+split_time = 1000
+time_train = time[:split_time]
+x_train = series[:split_time]
+time_valid = time[split_time:]
+x_valid = series[split_time:]
+
+class ResetStatesCallback(keras.callbacks.Callback):
+    def on_epoch_begin(self, epoch, logs):
+        self.model.reset_states()
+        
+keras.backend.clear_session()
+tf.random.set_seed(42)
+np.random.seed(42)
+
+window_size = 30
+train_set = sequential_window_dataset(x_train, window_size)
+
+model = keras.models.Sequential([
+  keras.layers.LSTM(100, return_sequences=True, stateful=True,
+                    batch_input_shape=[1, None, 1]),
+  keras.layers.LSTM(100, return_sequences=True, stateful=True),
+  keras.layers.Dense(1),
+  keras.layers.Lambda(lambda x: x * 200.0)
+])
+lr_schedule = keras.callbacks.LearningRateScheduler(
+    lambda epoch: 1e-8 * 10**(epoch / 20))
+reset_states = ResetStatesCallback()
+optimizer = keras.optimizers.SGD(lr=1e-8, momentum=0.9)
+model.compile(loss=keras.losses.Huber(),
+              optimizer=optimizer,
+              metrics=["mae"])
+history = model.fit(train_set, epochs=100,
+                    callbacks=[lr_schedule, reset_states])
+                    
+plt.semilogx(history.history["lr"], history.history["loss"])
+plt.axis([1e-8, 1e-4, 0, 30])
+
+keras.backend.clear_session()
+tf.random.set_seed(42)
+np.random.seed(42)
+
+window_size = 30
+train_set = sequential_window_dataset(x_train, window_size)
+valid_set = sequential_window_dataset(x_valid, window_size)
+
+model = keras.models.Sequential([
+  keras.layers.LSTM(100, return_sequences=True, stateful=True,
+                         batch_input_shape=[1, None, 1]),
+  keras.layers.LSTM(100, return_sequences=True, stateful=True),
+  keras.layers.Dense(1),
+  keras.layers.Lambda(lambda x: x * 200.0)
+])
+optimizer = keras.optimizers.SGD(lr=5e-7, momentum=0.9)
+model.compile(loss=keras.losses.Huber(),
+              optimizer=optimizer,
+              metrics=["mae"])
+reset_states = ResetStatesCallback()
+model_checkpoint = keras.callbacks.ModelCheckpoint(
+    "my_checkpoint.h5", save_best_only=True)
+early_stopping = keras.callbacks.EarlyStopping(patience=50)
+model.fit(train_set, epochs=500,
+          validation_data=valid_set,
+          callbacks=[early_stopping, model_checkpoint, reset_states])
+          
+model = keras.models.load_model("my_checkpoint.h5")
+
+rnn_forecast = model.predict(series[np.newaxis, :, np.newaxis])
+rnn_forecast = rnn_forecast[0, split_time - 1:-1, 0]
+
+plt.figure(figsize=(10, 6))
+plot_series(time_valid, x_valid)
+plot_series(time_valid, rnn_forecast)
+
+keras.metrics.mean_absolute_error(x_valid, rnn_forecast).numpy()
